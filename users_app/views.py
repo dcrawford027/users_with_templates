fab33a03b2bfe38e9264f0ae720927b23d0a1cc9
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-14 16:48:47 -0400

    completed table with database retrieval

--- users_app/views.py
+++ users_app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def addUser(request):
+    firstName = request.POST['first_name']
+    lastName = request.POST['last_name']
+    postEmail = request.POST['email']
+    postAge = request.POST['age']
+    newUser = User.objects.create(name=firstName + ' ' + lastName, email=postEmail, age=postAge)
+    return redirect('/')
